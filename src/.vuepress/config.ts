cca71e102b79a3cdc0950d1eb2e22adeaf4b4330
qqaazz2 <1093218847@qq.com>
2025-12-20 20:02:10 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/dreader-docs/",
 
   lang: "zh-CN",
   title: "DReader",
